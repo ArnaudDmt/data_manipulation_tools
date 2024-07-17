307e9ee09e03e91a172dcef6422423526060c01c
ArnaudDmt <dem.arnaud11@gmail.com>
2024-07-17 17:49:50 +0900

    routine working

--- routine.sh
+++ routine.sh
@@ -1,11 +1,21 @@
 #!/bin/zsh
 
+set -e
+
+
 cwd=$(pwd)
 
 
 rawDataPath="raw_data"
 outputDataPath="output_data"
 scriptsPath="scripts"
+
+yamlFile="$HOME/.config/mc_rtc/controllers/Passthrough.yaml"
+if [ ! -f "$yamlFile" ]; then
+    echo "The scripts excepts to find a configuration file named $yamlFile."
+    exit
+fi
+
 
 mocapLog="$rawDataPath/mocapData.csv"
 if [ -f "$mocapLog" ]; then
@@ -17,39 +27,30 @@ fi
 
 logReplayCSV="$outputDataPath/logReplay.csv"
 if [ -f "$logReplayCSV" ]; then
-    if [[ $(ag "mocap_worldBodyKine" $logReplayCSV) ]]; then
-        echo "The csv file of the replay with the observers has been found."
-    else
-        echo "The csv file of the replay with the observers has been found but does not contain the transformation from the body to the floating base. Please delete this file ($logReplayCSV) and re-run the script."
-        exit
-fi
+    echo "The csv file of the replay with the observers has been found."
 else
     logReplayBin="$outputDataPath/logReplay.bin"
     if [ -f "$logReplayBin" ]; then
         echo "The bin file of the replay with the observers has been found. Converting to csv."
-        mc_bin_to_log $logReplayBin
+        cd $outputDataPath
+        mc_bin_to_log "../$logReplayBin"
     else
         mcrtcLog="$rawDataPath/controllerLog.bin"
         if [ -f "$mcrtcLog" ]; then
-            echo "The log file of the controller was found."
-            yamlFile="$HOME/.config/mc_rtc/controllers/Passthrough.yaml"
-            if [ -f "$yamlFile" ]; then
-                echo "The configuration file for the replay with the observers was found."
-                if ! ag -i "MocapVisualizer" $yamlFile || ! ag -i "firstRun:" $yamlFile; then
-                    # Execute your action here if both patterns are found
-                    echo "Please add the MocapVisualizer observer to the list of the observers and check that the configuration firstRun exists, and that the name of the body is passed to the MCVanytEstimator under the category mocap/bodyName"
-                    exit
-                fi
-            else
-                echo "The scripts excepts to find a configuration file named $yamlFile if the complete log of the observers is not given."
+            echo "The log file of the controller was found. Replaying the log with the observer."
+            if ! ag -i "MocapVisualizer" $yamlFile || ! ag -i "firstRun:" $yamlFile || ! ag -i "bodyName:" $yamlFile; then
+                # Execute your action here if both patterns are found
+                echo "Please add the MocapVisualizer observer to the list of the observers and check that the configuration firstRun exists, and that the name of the body is passed to the MCVanytEstimator under the category mocap/bodyName"
                 exit
             fi
             sed -i "/^\([[:space:]]*firstRun: \).*/s//\1"true"/" $yamlFile
-            mc_rtc_ticker --replay-outputs -e -l $mcrtcLog
+            mc_rtc_ticker --no-sync --replay-outputs -e -l $mcrtcLog
             cd /tmp/
             LOG=$(find -iname "mc-control*" | grep "Passthrough" | grep ".bin" | tail -1)
-            mv $LOG $logReplayBin
-            mc_bin_to_log $logReplayBin
+            mv $LOG $cwd/$logReplayBin
+            cd $cwd/$outputDataPath
+            mc_bin_to_log logReplay.bin
+            cd $cwd
         else
             echo "The log file of the controller does not exist or is not named as expected. Expected: $mcrtcLog."
             exit
@@ -57,7 +58,8 @@ else
     fi
 fi
 
-logReplayCSV="$outputDataPath/logReplay.csv"
+
+cd $cwd
 
 # Prompt the user for input
 echo "Please enter the timestep of the controller in milliseconds: "
@@ -66,48 +68,72 @@ read timeStep
 echo "Please enter the time at which you want the pose of the mocap and the one of the observer must match: "
 read matchTime
 
-resampledMocapData="resampledMocapData.csv"
+# 
+resampledMocapData="$outputDataPath/resampledMocapData.csv"
+lightData="$outputDataPath/lightData.csv"
+realignedMocapLimbData="$outputDataPath/realignedMocapLimbData.csv"
+resultMocapLimbData="$outputDataPath/resultMocapLimbData.csv"
+
+
+
 if [ -f "$resampledMocapData" ]; then
     echo "The mocap's data has already been resampled. Using the existing data."
 else
     echo "Starting the resampling of the mocap's signal."
+    cd $cwd/$scriptsPath
     python resampleAndExtract_fromMocap.py "$timeStep" "False" "y"
     echo "Resampling of the mocap's signal completed."
 fi
+echo 
 
-lightData="$outputDataPath/lightData.csv"
+cd $cwd
+
+
 if [ -f "$lightData" ]; then
     echo "The light version of the observer's data has already been extracted. Using the existing data."
 else
     echo "Starting the extraction of the light version of the observer's data."
+    cd $cwd/$scriptsPath
     python extractLightReplayVersion.py
     echo "Extraction of the light version of the observer's data completed."
 fi
+echo 
 
-realignedMocapLimbData="$outputDataPath/realignedMocapLimbData.csv"
+cd $cwd
+
+
 if [ -f "$realignedMocapLimbData" ]; then
     echo "The temporally aligned version of the mocap's data already exists. Using the existing data."
 else
     echo "Starting the cross correlation for temporal data alignement."
+    cd $cwd/$scriptsPath
     python crossCorrelation.py "$timeStep" "False" "y"
     echo "Temporal alignement of the mocap's data with the observer's data completed."
 fi
+echo 
 
-resultMocapLimbData="$outputDataPath/resultMocapLimbData.csv"
+cd $cwd
+
+
 if [ -f "$resultMocapLimbData" ]; then
     echo "The mocap's data has already been completely treated. Using the existing data."
 else
     echo "Matching the pose of the mocap with the pose of the observer at $matchTime seconds."
+    cd $cwd/$scriptsPath
     python matchInitPose.py "$matchTime" "False" "y"
     echo "Matching of the pose of the mocap with the pose of the observer completed."
 fi
+echo 
 
+cd $cwd
 
 
 echo "Do you want to replay the log with the obtained mocap's data?"
 select replayWithMocap in "Yes" "No"; do
     case $replayWithMocap in
-        Yes ) mcrtcLog="$rawDataPath/controllerLog.bin"; sed -i "/^\([[:space:]]*firstRun: \).*/s//\1"false"/" $yamlFile; mc_rtc_ticker --replay-outputs -e -l $mcrtcLog; break;;
+        Yes ) mcrtcLog="$rawDataPath/controllerLog.bin"; pwd; echo $mcrtcLog ; sed -i "/^\([[:space:]]*firstRun: \).*/s//\1"false"/" $yamlFile; mc_rtc_ticker --no-sync --replay-outputs -e -l $mcrtcLog; break;;
         No ) exit;;
     esac
 done
+
+echo "The pipeline finished without any issue. If you are not satisfied with the result, please re-run the scripts one by one and help yourself with the logs for the debug. Please also make sure that the time you set for the matching of the mocap and the observer is correct."
