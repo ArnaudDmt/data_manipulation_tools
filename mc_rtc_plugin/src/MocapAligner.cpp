afeac1eb49ab81f3db6f873963c7f210e67ef797
ArnaudDmt <dem.arnaud11@gmail.com>
2024-07-18 17:53:28 +0900

    removed useless prompt

--- mc_rtc_plugin/src/MocapAligner.cpp
+++ mc_rtc_plugin/src/MocapAligner.cpp
@@ -10,29 +10,19 @@ MocapAligner::~MocapAligner() = default;
 
 void MocapAligner::init(mc_control::MCGlobalController & controller, const mc_rtc::Configuration & config)
 {
-  mc_rtc::log::info("MocapAligner::init called with configuration:\n{}", config.dump(true, true));
-
   robot_ = config("robot", controller.controller().robot().name());
-  config("bodyName", mocapBodyName_);
+  mocapBodyName_ = static_cast<std::string>(config("bodyName"));
 
   auto & logger = (const_cast<mc_control::MCController &>(controller.controller())).logger();
   mc_state_observation::conversions::kinematics::addToLogger(logger, worldMocapBodyKine_, "MocapAligner_worldBodyKine");
 }
 
-void MocapAligner::reset(mc_control::MCGlobalController & controller)
-{
-  mc_rtc::log::info("MocapAligner::reset called");
-}
+void MocapAligner::reset(mc_control::MCGlobalController & controller) {}
 
-void MocapAligner::before(mc_control::MCGlobalController &)
-{
-  mc_rtc::log::info("MocapAligner::before");
-}
+void MocapAligner::before(mc_control::MCGlobalController &) {}
 
 void MocapAligner::after(mc_control::MCGlobalController & controller)
 {
-  mc_rtc::log::info("MocapAligner::after");
-
   worldMocapBodyKine_ = mc_state_observation::conversions::kinematics::fromSva(
       controller.controller().realRobot(robot_).bodyPosW(mocapBodyName_),
       stateObservation::kine::Kinematics::Flags::pose);
